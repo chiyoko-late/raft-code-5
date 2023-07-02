0cca1eddbca450faff850b5787bc0f1f33b72b62
chiyoko-late <hajime96suke@gmail.com>
2023-07-03 04:52:06 +0900

    no message

--- appendentries.h
+++ appendentries.h
@@ -22,8 +22,8 @@
 
 #define SERVER_ADDR "0.0.0.0"
 #define STRING (10LL)
-#define ALL_ACCEPTED_ENTRIES (1000L)
-#define ENTRY_NUM (100LL)
+#define ALL_ACCEPTED_ENTRIES (10000L)
+#define ENTRY_NUM (1000LL)
 
 // using namespace std;
 
