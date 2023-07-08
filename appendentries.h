a2250e00f7acc5759a8f1461072ec87d774563eb
chiyoko-late <93936783+chiyoko-late@users.noreply.github.com>
2023-07-08 11:12:04 +0900

    Update appendentries.h

--- appendentries.h
+++ appendentries.h
@@ -24,7 +24,7 @@
 #define SERVER_ADDR "0.0.0.0"
 #define STRING (10LL)
 #define ALL_ACCEPTED_ENTRIES (100000L * 1000)
-#define ENTRY_NUM (100000L)
+#define ENTRY_NUM (100000L * 500)
 
 // using namespace std;
 
